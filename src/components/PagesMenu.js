58c0cce9409d0e76c27e45dfcee4cf61c0f07fbb
naiiiden <na4ko359@gmail.com>
2022-05-29 07:26:25 +0300

    pages menu component

---
+++ src/components/PagesMenu.js
@@ -0,0 +1,13 @@
+const PagesMenu = () => {
+    return (    
+        <nav className="pages--nav">
+            <ul className="pages--nav--ul">
+                <li><a href="" className="pages--link">About</a></li>
+                <li><a href="" className="pages--link">Jobs</a></li>
+                <li><a href="" className="pages--link">Apprenticeships</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default PagesMenu;
