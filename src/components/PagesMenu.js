301ab3fefc98ac5d5778d246e20bddc4b4ff08f7
naiiiden <na4ko359@gmail.com>
2022-05-30 20:32:10 +0300

    react-router6 test3

--- src/components/PagesMenu.js
+++ src/components/PagesMenu.js
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const PagesMenu = () => {
     return (    
         <nav className="pages--nav">
             <ul className="pages--nav--ul">
-                <li><Link to="/" className="pages--link">About</Link></li>
-                <li><Link to="/jobs" className="pages--link">Jobs</Link></li>
-                <li><Link to="/apprenticeships" className="pages--link">Apprenticeships</Link></li>
+                <li><NavLink className={`pages--link ${({ isActive }) => isActive ? "active" : ""}`} to="/">About</NavLink></li>
+                <li><NavLink to="/jobs" className={`pages--link ${({ isActive }) => isActive ? "active" : ""}`}>Jobs</NavLink></li>
+                <li><NavLink to="/apprenticeships" className={`pages--link ${({ isActive }) => isActive ? "active" : ""}`}>Apprenticeships</NavLink></li>
             </ul>
         </nav>
     )
