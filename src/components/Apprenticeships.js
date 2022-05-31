10434f7901b6254a56a68d2c6a607bb741f21832
naiiiden <na4ko359@gmail.com>
2022-05-31 20:56:39 +0300

    apprenticeships component

--- src/components/Apprenticeships.js
+++ src/components/Apprenticeships.js
@@ -1,6 +1,14 @@
 const Apprenticeships = () => {
     return (
-        <p>hey3</p>
+        <main>
+            <h1>Apprenticeships at Ableton</h1>
+            <h2>What are they and what do you need to get started?</h2>
+            <p>Dual apprenticeship programs have a long tradition in Germany, and they play an important role in the country’s economy. The programs are a proven stepping stone towards a meaningful career without the need for a university degree. They give you the chance to apply theoretical knowledge learned in a vocational school to tasks in the workplace, while you divide your time between class and office days.</p>
+            <img src="https://cdn-resources.ableton.com/resources/filer_thumbnails/unsorted-uploads/azubi_photos_3_1.jpg__920.0x613.0_q85_subsampling-2.jpg" alt=""/>
+            <p>At Ableton, you'll work towards the Office Management Assistant apprenticeship over two and a half years, developing your professional and personal skills in different teams. We look for people who are highly motivated to work in a commercial office environment, with an excellent level of German and very good English. It’s important that you’re eager to learn, organized in your work, and communicative when collaborating with others. Plus, you should be ready to get the most out of your development through giving and receiving feedback. You can expect a relaxed, professional atmosphere with a dynamic team of people who love music in return.</p>
+            <p>If this sounds like you, feel free to have a look at our detailed information in German <a href="">here</a>. And please share this page with anyone who might be interested to help us find our future colleagues!</p>
+            <img src="https://cdn-resources.ableton.com/resources/filer_thumbnails/unsorted-uploads/azubi_photos_3_2.jpg__920.0x613.0_q85_subsampling-2.jpg" alt=""/>
+        </main>
     )
 }
 
