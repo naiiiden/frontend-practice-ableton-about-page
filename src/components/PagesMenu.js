34761b55a2aa52240146e3435c77f172e972966d
naiiiden <na4ko359@gmail.com>
2022-06-06 08:52:27 +0300

    2nd nav menu hides while  scrolling down and show again when scrolling up

--- src/components/PagesMenu.js
+++ src/components/PagesMenu.js
@@ -1,6 +1,18 @@
 import { NavLink } from "react-router-dom";
 
 const PagesMenu = () => {
+
+    {var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".pages--nav").style.top = "0";
+    } else {
+        document.querySelector(".pages--nav").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+    }}
+
     return (    
         <nav className="pages--nav">
             <ul className="pages--nav--ul">
